a35fbee2172d4412c9ef9270436131ff7a9d670d
Jakob Wolitzki <jakob.wolitzki@gmail.com>
2019-04-12 23:23:46 +0200

    back

--- back/allpaczka/allpaczka/settings.py
+++ back/allpaczka/allpaczka/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'timecalculator',
     'rest_framework',
-    'sortedm2m',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
