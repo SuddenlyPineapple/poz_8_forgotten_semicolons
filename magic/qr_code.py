d6416bccc98202b939fd872f48b3253f219363b5
danielcieslinski <daniel98c@gmail.com>
2019-04-13 09:00:39 +0200

    QR

--- magic/qr_code.py
+++ magic/qr_code.py
@@ -1,9 +1,14 @@
 import qrcode.image.svg
+import qrtools
 
 def gen_qrcode(code):
+    code = str(code)
     factory = qrcode.image.svg.SvgPathImage
     img = qrcode.make(str(code), image_factory=factory)
-    img.save(str(code)+'.svg')
+    img.save(code+'.svg')
 
-def decode_qrcode(image):
-    pass
+
+def decode_qrcode(image_link):
+    qr = qrtools.QR()
+    qr.decode(image_link)
+    return qr.data
