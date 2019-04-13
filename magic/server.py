43358d6ada6ddc14842bd2fbea1f49f668d38988
Max Adamski <maxadamski@gmx.com>
2019-04-13 05:45:13 +0200

    ...

--- magic/server.py
+++ magic/server.py
@@ -3,8 +3,10 @@ from sys import stderr, argv
 from pprint import pprint
 from daniel import new_route
 from flask import Flask, jsonify, request, abort, make_response
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 db = {}
 
 
